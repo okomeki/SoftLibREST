b439ae7a9a40f188fa94881820d725d265f939a5
okome <okome@siisise.net>
2020-11-06 06:05:58 +0900

    json2系

--- src/main/java/net/siisise/net/rest/RestClient.java
+++ src/main/java/net/siisise/net/rest/RestClient.java
@@ -8,6 +8,8 @@ import java.net.URISyntaxException;
 import net.siisise.io.FileIO;
 import net.siisise.json.JSON;
 import net.siisise.json.JSONValue;
+import net.siisise.json2.JSON2;
+import net.siisise.json2.JSON2Value;
 
 /**
  * JSONを使うのでSoftLibに置けない仮置き場
@@ -34,6 +36,10 @@ public class RestClient {
         return get(new URI(baseuri + uri));
     }
 
+    public JSON2Value get2(String uri) throws IOException, URISyntaxException {
+        return get2(new URI(baseuri + uri));
+    }
+
     /**
      * HTTP GET パラメータ未対応
      *
@@ -52,9 +58,32 @@ public class RestClient {
 
         return result(conn);
     }
+
+    /**
+     * HTTP GET パラメータ未対応
+     *
+     * @param uri
+     * @return JSON固定 bindとかしない
+     * @throws IOException
+     */
+    public JSON2Value get2(URI uri) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) uri.toURL().openConnection();
+
+        conn.setRequestProperty("Accept", "application/json");
+        if (accessToken != null) {
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        }
+        conn.connect();
+
+        return result2(conn);
+    }
     
     public JSONValue post(String uri, String... parameters) throws IOException, URISyntaxException {
         return post(new URI(baseuri + uri), parameters);
+    }
+
+    public JSON2Value post2(String uri, String... parameters) throws IOException, URISyntaxException {
+        return post2(new URI(baseuri + uri), parameters);
     }
 
     /**
@@ -97,9 +126,55 @@ public class RestClient {
         return result(conn);
     }
     
+    /**
+     * POST. パラメータのエンコードしてない.
+     *
+     * @param uri
+     * @param parameters エンコードしてない
+     * @return
+     * @throws IOException
+     */
+    public JSON2Value post2(URI uri, String... parameters) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) uri.toURL().openConnection();
+        // https限定
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json");
+        if (accessToken != null) {
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        }
+        if (parameters.length > 0) {
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < parameters.length; i += 2) {
+                if (sb.length() > 0) {
+                    sb.append("&");
+                }
+                sb.append(parameters[i]);
+                sb.append("=");
+                sb.append(parameters[i + 1]);
+            }
+            byte[] utf = sb.toString().getBytes("utf-8");
+            conn.setRequestProperty("Content-Length", "" + utf.length);
+            OutputStream out = conn.getOutputStream();
+            out.write(utf);
+            out.flush();
+        }
+
+        conn.connect();
+
+        return result2(conn);
+    }
+
     private JSONValue result(HttpURLConnection conn) throws IOException {
         byte[] result = FileIO.binRead(conn.getInputStream());
         conn.disconnect();
         return JSON.parse(result);
+    }
+
+    private JSON2Value result2(HttpURLConnection conn) throws IOException {
+        byte[] result = FileIO.binRead(conn.getInputStream());
+        conn.disconnect();
+        return (JSON2Value) JSON2.parse(result);
     }
 }
