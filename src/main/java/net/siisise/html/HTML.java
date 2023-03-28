4e3df9b71bdefd0c2b84705684d50db4b0310d60
okomeki <siisise@gmail.com>
2023-03-28 22:48:18 +0900

    HTMLっぽいところを分ける

---
+++ src/main/java/net/siisise/html/HTML.java
@@ -0,0 +1,37 @@
+package net.siisise.html;
+
+/**
+ * HTMLのエスケープなど.
+ * どこに置くかは未定.
+ */
+public class HTML {
+    
+    public static String decode(char ch) {
+        switch(ch) {
+            case '&':
+                return "&amp;";
+            case '<':
+                return "&lt;";
+            case '>':
+                return "&gt;";
+            case '"':
+                return "&quot;";
+            case '\'':
+                return "&#39;";
+            case ' ':
+                return "&nbsp;";
+            default:
+                return String.valueOf(ch);
+        }
+    }
+    
+    public static String esc(String src) {
+        StringBuilder sb = new StringBuilder();
+        char[] chs = src.toCharArray();
+        for ( char ch : chs ) {
+            String dec = decode(ch);
+            sb.append(dec);
+        }
+        return sb.toString();
+    }
+}
